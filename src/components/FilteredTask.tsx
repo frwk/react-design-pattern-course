c463057a061eec737eb3fa8df3036653b7b792b1
rklf <11515950+rklf@users.noreply.github.com>
2023-10-06 16:31:14 +0200

    Refactoring most of class components into functional components

--- src/components/FilteredTask.tsx
+++ src/components/FilteredTask.tsx
@@ -1,40 +1,39 @@
-import React from 'react';
-import {Button} from "@mui/material";
-import FilteredTaskProps from "../types/FilteredTaskProps.ts";
+import { useState } from 'react';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import FilteredTaskProps from '../types/FilteredTaskProps';
 
-class FilteredTaskList extends React.Component<FilteredTaskProps> {
-    state = {
-        tasks: [],
-        filter: 'all', // par défaut, affiche toutes les tâches
-    };
+const FilteredTaskList: React.FC<FilteredTaskProps> = ({ tasks, render }) => {
+    const [filter, setFilter] = useState('all'); // par défaut, affiche toutes les tâches
 
-    setFilter = (filter: string) => {
-        this.setState({ filter });
-    };
-
-    getFilteredTasks = () => {
-        switch (this.state.filter) {
+    const getFilteredTasks = () => {
+        switch (filter) {
             case 'completed':
-                return this.props.tasks.filter(task => task.completed);
+                return tasks.filter(task => task.completed);
             case 'notCompleted':
-                return this.props.tasks.filter(task => !task.completed);
+                return tasks.filter(task => !task.completed);
             default:
-                return this.props.tasks;
+                return tasks;
         }
     };
 
-    render() {
-        return (
-            <>
-                <div className='flex justify-center gap-2'>
-                    <Button variant='contained' color='success' onClick={() => this.setFilter('all')}>Toutes les tâches</Button>
-                    <Button variant='contained' color='primary' onClick={() => this.setFilter('completed')}>Complètes</Button>
-                    <Button variant='contained' color='error' onClick={() => this.setFilter('notCompleted')}>Incomplètes</Button>
-                </div>
-                { this.props.render(this.getFilteredTasks())}
-            </>
-        );
-    }
-}
+    return (
+        <>
+            <FormControl fullWidth>
+                <InputLabel id="filter-select-label">Filtre</InputLabel>
+                <Select
+                    labelId="filter-select-label"
+                    value={filter}
+                    label="Filtre"
+                    onChange={(e) => setFilter(e.target.value as string)}
+                >
+                    <MenuItem value='all'>Tout</MenuItem>
+                    <MenuItem value='completed'>Completées</MenuItem>
+                    <MenuItem value='notCompleted'>Non completées</MenuItem>
+                </Select>
+            </FormControl>
+            {render(getFilteredTasks())}
+        </>
+    );
+};
 
 export default FilteredTaskList;
--- src/components/TaskGridView.tsx
+++ src/components/TaskGridView.tsx
@@ -1,42 +1,37 @@
-import React from 'react';
 import TaskListViewProps from '../types/TaskListViewProps';
 import { Button, Card, CardActions, CardContent, Grid } from '@mui/material';
 
-class TaskGridView extends React.Component<TaskListViewProps> {
+const TaskGridView: React.FC<TaskListViewProps> = ({ tasks, onAdd, onDelete, onToggle, }) => {
 
-    handleAddTask = () => {
+    const handleAddTask = () => {
         const title = prompt('Entrez le nom de la nouvelle tâche:');
         if (title) {
-            this.props.onAdd(title);
+            onAdd(title);
         }
     };
 
-    render() {
-        const { tasks, onDelete, onToggle } = this.props;
-
-        return (
-            <div className='flex flex-col items-center gap-4'>
-                <Button variant="contained" onClick={this.handleAddTask}>Ajouter une tâche</Button>
-                <Grid container spacing={2}>
-                    {tasks.map(task => (
-                        <Grid item xs={12} sm={6} lg={4} key={task.id}>
-                            <Card variant="outlined" className={task.completed ? 'bg-green-500' : ''}>
-                                <CardContent>
-                                    {task.title}
-                                </CardContent>
-                                <CardActions>
-                                    <Button size="small" variant="contained" color="error" onClick={() => onDelete(task.id)}>Supprimer</Button>
-                                    <Button size="small" variant="contained" color={task.completed ? 'error' : 'success'} onClick={() => onToggle(task.id)}>
-                                        {task.completed ? 'Annuler' : 'Terminer'}
-                                    </Button>
-                                </CardActions>
-                            </Card>
-                        </Grid>
-                    ))}
-                </Grid>
-            </div>
-        );
-    }
-}
+    return (
+        <div className='flex flex-col items-center gap-4'>
+            <Button variant="contained" onClick={handleAddTask}>Ajouter une tâche</Button>
+            <Grid container spacing={2}>
+                {tasks.map(task => (
+                    <Grid item xs={12} sm={6} lg={4} key={task.id}>
+                        <Card variant="outlined" className={task.completed ? 'bg-green-500' : ''}>
+                            <CardContent>
+                                {task.title}
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small" variant="contained" color="error" onClick={() => onDelete(task.id)}>Supprimer</Button>
+                                <Button size="small" variant="contained" color={task.completed ? 'error' : 'success'} onClick={() => onToggle(task.id)}>
+                                    {task.completed ? 'Annuler' : 'Terminer'}
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+        </div>
+    );
+};
 
 export default TaskGridView;
