642fd3ced2a7416c44b41f9b6e98ee8459f7a163
rklf <11515950+rklf@users.noreply.github.com>
2023-10-06 21:38:25 +0200

    Refactoring FilteredTask into a useTaskFilter custom hook

--- src/components/TaskListContainer.tsx
+++ src/components/TaskListContainer.tsx
@@ -1,35 +1,34 @@
-import Task from '../types/Task';
 import FilteredTask from "../components/FilteredTask";
 import TaskListView from "../components/TaskListView";
 import usePagination from '../hooks/usePagination';
 import Pagination from './Pagination';
 import useTaskManager from '../hooks/useTaskManager';
+import useTaskFilter from '../hooks/useTaskFilter';
 
 const TaskListContainer = () => {
     const { tasks, addTask, removeTask, updateTask, isLoading } = useTaskManager();
+    const { filteredTasks, filter, setFilter } = useTaskFilter(tasks);
+    const { slicedItems, currentPage, totalPages, handlePageChange } = usePagination(filteredTasks, 10);
 
     return (
-        <FilteredTask tasks={tasks} render={(filteredTasks: Task[]) => {
-            const { slicedItems, currentPage, totalPages, handlePageChange } = usePagination(filteredTasks, 10);
-            return (
-                <>
-                    <TaskListView
-                        tasks={slicedItems}
-                        onAdd={addTask}
-                        onDelete={removeTask}
-                        onToggle={updateTask}
-                        isLoading={isLoading}
+        <FilteredTask filter={filter} setFilter={setFilter}>
+            <>
+                <TaskListView
+                    tasks={slicedItems}
+                    onAdd={addTask}
+                    onDelete={removeTask}
+                    onToggle={updateTask}
+                    isLoading={isLoading}
+                />
+                {isLoading ? null : (
+                    <Pagination
+                        currentPage={currentPage}
+                        totalPages={totalPages}
+                        onPageChange={handlePageChange}
                     />
-                    {isLoading ? null : (
-                        <Pagination
-                            currentPage={currentPage}
-                            totalPages={totalPages}
-                            onPageChange={handlePageChange}
-                        />
-                    )}
-                </>
-            )
-        }} />
+                )}
+            </>
+        </FilteredTask>
     );
 };
 
