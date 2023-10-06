77b7caa70b96652e536177c81e9fc14947f1a390
Florian Perrier <florian.fp.perrier@gmail.com>
2023-10-06 16:21:20 +0200

    add hoc pattern

--- src/components/TaskListView.tsx
+++ src/components/TaskListView.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import TaskListViewProps from '../types/TaskListViewProps';
 import { Button, Checkbox, IconButton, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
+import withPagination from './HOC/withPagination';
+import PaginationProps from '../types/PaginationProps';
 
-class TaskListView extends React.Component<TaskListViewProps> {
+class TaskListView extends React.Component<TaskListViewProps & Partial<PaginationProps>> {
 
     handleAddTask = () => {
         const title = prompt('Entrez le nom de la nouvelle tâche:');
@@ -13,13 +15,17 @@ class TaskListView extends React.Component<TaskListViewProps> {
     };
 
     render() {
-        const { tasks, onDelete, onToggle } = this.props;
+        const { tasks, onDelete, onToggle, currentPage, itemsPerPage } = this.props;
+
+        const displayedTasks = (currentPage && itemsPerPage) ?
+            tasks.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage) :
+            tasks;
 
         return (
             <div className='flex flex-col items-center'>
                 <Button variant="contained" onClick={this.handleAddTask}>Ajouter une tâche</Button>
                 <List>
-                    {tasks.map(task => (
+                    {displayedTasks.map(task => (
                         <ListItem key={task.id}>
                             <ListItemIcon>
                                 <Checkbox
@@ -46,4 +52,4 @@ class TaskListView extends React.Component<TaskListViewProps> {
     }
 }
 
-export default TaskListView;
+export default withPagination(TaskListView);
