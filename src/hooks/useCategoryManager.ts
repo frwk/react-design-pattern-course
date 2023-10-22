e089d103f433373dd85094af51b278e2f8016b20
Wendy Afrim <wendy.afrim2@gmail.com>
2023-10-22 09:58:29 +0200

    feat(part-6): Implementing flyweight design pattern

---
+++ src/hooks/useCategoryManager.ts
@@ -0,0 +1,25 @@
+import Category from "../types/Category.ts";
+import {useState} from "react";
+
+const useCategoryManager = () => {
+    const [categories, setCategories] = useState<Category[]>([]);
+
+    const addCategory = (category: Category) => {
+        const existingCategory = categories.map(cat => {
+            return cat.title === category.title
+        })
+
+        if (existingCategory) {
+            return existingCategory;
+        }
+
+        setCategories([...categories, category]);
+    }
+
+    return {
+        categories,
+        addCategory
+    }
+}
+
+export default useCategoryManager;
--- src/types/Task.ts
+++ src/types/Task.ts
@@ -1,6 +1,7 @@
 export default interface Task {
     id: number;
     title: string;
+    category: string|null
     completed: boolean;
     userId: number;
 }
