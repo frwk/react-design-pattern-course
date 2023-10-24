28cf3cd0484bf798ec388e3af3804a76d7e557f0
rklf <11515950+rklf@users.noreply.github.com>
2023-10-24 11:43:04 +0200

    Add filters on categories

--- src/hooks/useCategoryManager.ts
+++ src/hooks/useCategoryManager.ts
@@ -1,8 +1,20 @@
 import Category from "../types/Category.ts";
-import {useState} from "react";
+import { useMemo, useState } from "react";
+import Task from "../types/Task.ts";
 
-const useCategoryManager = () => {
+const useCategoryManager = (tasks: Task[]) => {
     const [categories, setCategories] = useState<Category[]>([]);
+
+    useMemo(() => {
+        const categories = tasks.reduce((acc: Category[], task: Task) => {
+            if (!task.category) return acc;
+            const existingCategory = acc.some((cat) => cat.title === task.category);
+            if (existingCategory) return acc;
+            return [...acc, { title: task.category }];
+        }, []);
+
+        setCategories(categories);
+    }, [tasks]);
 
     const addCategory = (category: Category) => {
         const existingCategory = categories.some((cat) => cat.title === category.title);
--- src/hooks/useTaskFilter.ts
+++ src/hooks/useTaskFilter.ts
@@ -1,33 +1,60 @@
+import Category from "../types/Category";
 import Task from "../types/Task";
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
+
+interface FilterState {
+  completed: boolean | null;
+  categories: Category[];
+}
+
+const filterReducer = (state: FilterState, action: { type: string, payload: any }) => {
+  const categoryTitle = action.payload;
+  const updatedCategories = new Set(state.categories.map(category => category.title));
+
+  switch (action.type) {
+    case 'SET_COMPLETION':
+      return {
+        ...state,
+        completed: action.payload,
+      };
+    case 'TOGGLE_CATEGORY':
+      if (updatedCategories.has(categoryTitle)) {
+        updatedCategories.delete(categoryTitle);
+      } else {
+        updatedCategories.add(categoryTitle);
+      }
+      return {
+        ...state,
+        categories: Array.from(updatedCategories).map(title => ({ title })),
+      };
+    case 'REMOVE_NON_EXISTING_CATEGORIES':
+      return {
+        ...state,
+        categories: state.categories.filter(category => action.payload.some((task: Task) => task.category === category.title)),
+      };
+    default:
+      return state;
+  }
+}
 
 const useFilter = (tasks: Task[]) => {
-  const [filter, setFilter] = useState('all'); // par défaut, affiche toutes les tâches
+  const [filter, dispatch] = useReducer(filterReducer, { completed: null, categories: [] });
   const [filteredTasks, setFilteredTasks] = useState(tasks);
 
   useEffect(() => {
-    const filterTasks = () => {
-      switch (filter) {
-        case 'completed':
-          setFilteredTasks(tasks.filter(task => task.completed));
-          break;
-        case 'notCompleted':
-          setFilteredTasks(tasks.filter(task => !task.completed));
-          break;
-        default:
-          setFilteredTasks(tasks);
-      }
-    }
-    filterTasks();
+    dispatch({ type: 'REMOVE_NON_EXISTING_CATEGORIES', payload: tasks });
+    const filteredTasks = tasks.filter((task) => filter.completed === null || filter.completed === task.completed).filter((task) => filter.categories.length === 0 || filter.categories.some((category) => task.category === category.title));
+    setFilteredTasks(filteredTasks);
 
     return () => {
       setFilteredTasks([]);
     }
   }, [filter, tasks]);
 
+
   return {
     filter,
-    setFilter,
+    dispatch,
     filteredTasks,
   };
 }
